8e2838545551d38ded5fe6cc8279895e88a175ab
David Whitlark <whitlark@gmail.com>
2015-06-20 11:53:03 -0400

    I guess nvd3 sparkline model can't animate :<

--- ui/chat/assets/javascripts/app/messagesparkline.jsx
+++ ui/chat/assets/javascripts/app/messagesparkline.jsx
@@ -23,27 +23,22 @@ define(function (require, exports, module) {
             var now = moment(),
                 minutes = { };
 
+            // 24 hours in minutes
+            var scale = 0.05,
+                range = 1440;
+
             this.state.messages.forEach(function (message) {
                 var ago = now.diff(message.when, 'minutes');
-                ago = Math.floor(ago * 0.02);
+                ago = Math.floor(ago * scale);
                 minutes[ago] = (minutes[ago] || 0) + 1;
             });
 
-            var range = Object.keys(minutes).map(function (ago) {
-                return parseFloat(ago);
-            });
-            if (!range.length) return [{ x: 0, y: 0 }, { x: 1, y: 0 }];
-
-            range.sort(function (a, b) {
-                return a - b;
-            });
-
             var x = 0,
-                data = [ ],
                 start = 0,
-                stop = range[range.length - 1];
+                data = [ ],
+                stop = Math.floor(range * scale);
 
-            for (var ago=start; ago <= stop; ++ago) {
+            for (var ago=stop; ago >= start; --ago) {
                 data.push({
                     x: ++x,
                     y: minutes[ago] || 0
@@ -60,35 +55,42 @@ define(function (require, exports, module) {
             return data;
         },
 
-        sparkline: function () {
-            var data = this.messages(),
-                dom = this.getDOMNode(),
-                color = this.props.color,
+        model: function () {
+            var color = this.props.color,
                 width = this.props.width;
-                
-            nv.addGraph({
-                generate: function() {
-                    var chart = nv.models.sparkline()
-                        .width(width)
-                        .height(15);
+            if (!this.chartModel) {
+                this.chartModel = nv.models.sparkline()
+                    .width(width)
+                    .height(15);
+            }
+            return this.chartModel;
+        },
 
-                    d3.select(dom)
-                        .datum(data)
-                        .call(chart);
+        vis: function () {
+            var dom = this.getDOMNode();
+            if (!this.chartVis) {
+                this.chartVis = d3.select(dom);
+            }
+            return this.chartVis;
+        },
 
-                    return chart;
-                }
-            });            
+        sparkline: function (update) {
+            var vis = this.vis(),
+                model = this.model(),
+                data = this.messages();
+
+            vis.datum(data)
+                .call(model);
         },
 
         componentDidMount: function () {
             clearTimeout(this.trigger);
-            this.trigger = setTimeout(this.sparkline, 500);
+            this.trigger = setTimeout(this.sparkline, 100);
         },
 
-        componentDidUpdate: function () {
+        componentDidUpdate: function (update) {
             clearTimeout(this.trigger);
-            this.trigger = setTimeout(this.sparkline, 500);
+            this.trigger = setTimeout(this.sparkline, 100);
         },
 
         render: function () {

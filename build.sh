c92925548b5c249df92119bca956dfb4093376db
David Whitlark <whitlark@gmail.com>
2015-06-29 07:33:53 -0400

    added param to build.sh so you can put your username

--- build.sh
+++ build.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 pushd ./util/barrier
-docker build -t goldbuick/util-barrier .
+docker build -t $1/util-barrier .
 popd
 
 pushd ./util/proxy
-docker build -t goldbuick/util-proxy .
+docker build -t $1/util-proxy .
 popd
 
 pushd ./util/static
-docker build -t goldbuick/util-static .
+docker build -t $1/util-static .
 popd
 
 pushd ./stem
-docker build -f api-ident.Dockerfile -t goldbuick/stem-api-ident .
-docker build -f api-irc.Dockerfile -t goldbuick/stem-api-irc .
-docker build -f base.Dockerfile -t goldbuick/stem-base .
-docker build -f pass-log.Dockerfile -t goldbuick/stem-pass-log .
+docker build -f api-ident.Dockerfile -t $1/stem-api-ident .
+docker build -f api-irc.Dockerfile -t $1/stem-api-irc .
+docker build -f base.Dockerfile -t $1/stem-base .
+docker build -f pass-log.Dockerfile -t $1/stem-pass-log .
 popd
 
 pushd ./ui/chat
 make clean build
-docker build -t goldbuick/ui-chat .
+docker build -t $1/ui-chat .
 popd
 
 pushd ./ui/config
 make clean build
-docker build -t goldbuick/ui-config .
+docker build -t $1/ui-config .
 popd
 
 pushd ./ui/uplink
 make clean build
-docker build -t goldbuick/ui-uplink .
+docker build -t $1/ui-uplink .
 popd
 
 docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")

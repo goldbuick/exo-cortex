338f5e1bc511abc71e0e6bcc6c214c4835240894
David Whitlark <whitlark@gmail.com>
2015-07-06 07:33:44 -0400

    functioning username completion when typing '@'

--- ui/chat/assets/javascripts/app/message-reply.jsx
+++ ui/chat/assets/javascripts/app/message-reply.jsx
@@ -3,25 +3,45 @@ define(function (require, exports, module) {
 
     var ChannelStore = require('app/channel-store'),
         MessageActions = require('app/message-actions');
-
+        
     var MessageReply = React.createClass({
         mixins: [
             Reflux.connect(ChannelStore, 'channel')
         ],
+
+        userList: function () {
+            if (this.props.server &&
+                this.props.channel &&
+                this.state.channel[this.props.server][this.props.channel] &&
+                this.state.channel[this.props.server][this.props.channel].users) {
+
+                return this.state.channel[this.props.server][this.props.channel].users;
+            }
+            return [ ];
+        },
         
-        getKey: function (op) {
+        stateKey: function (op) {
             return this.props.server + ':' + this.props.channel + ':' + op;
         },
 
-        getValue: function (op) {
-            var key = this.getKey(op);
-            return this.state[key] || '';
+        stateValue: function (op, value) {
+            var key = this.stateKey(op);
+            return this.state[key] || value;
         },
 
-        getCurrentWord: function (e) {
+        wordsKey: function () {
+            return this.stateKey('words');
+        },
+
+        inputKey: function () {
+            return this.stateKey('input');
+        },
+
+        currentWord: function () {
             var front,
-                index = e.target.selectionStart,
-                words = e.target.value.split(' ');
+                input = this.refs.reply.getDOMNode(),
+                index = input.selectionStart,
+                words = input.value.split(' ');
             
             while (index > 0 && words.length) {
                 front = words.shift();
@@ -31,39 +51,47 @@ define(function (require, exports, module) {
             return front;
         },
 
-        getTabList: function (word) {
-            if (word &&
-                this.props.server &&
-                this.props.channel &&
-                this.state.channel[this.props.server][this.props.channel] &&
-                this.state.channel[this.props.server][this.props.channel].users) {
+        replaceCurrentWord: function (value) {
+            var input = this.refs.reply.getDOMNode(),
+                index = input.selectionStart,
+                words = input.value.split(' ');
 
-                var users = this.state.channel[this.props.server][this.props.channel].users;
-                return users.filter(function (user) {
-                    return user.toLowerCase().indexOf(word.toLowerCase()) === 0;
-                });
+            for (var i=0; i < words.length; ++i) {
+                index -= (words[i].length + 1);
+                if (index <= 0) {
+                    words[i] = value + (i === 0 ? ': ' : '');
+                    break;
+                }
             }
 
-            return [ ];
+            return words.join(' ');
+        },
+
+        wordList: function (word) {
+            return this.userList().filter(function (user) {
+                return user.toLowerCase().indexOf(word.toLowerCase()) !== -1;
+            });
         },
 
         onChange: function (e) {
-            var key = this.getKey('input');
+            var key = this.inputKey();
             e.preventDefault();
             this.state[key] = e.target.value;
             this.setState(this.state);
         },
 
         onKeyDown: function (e) {
-            var inputKey = this.getKey('input'),
-                tabKey = this.getKey('tab');
+            var inputKey = this.inputKey(),
+                wordsKey = this.wordsKey(),
+                input = this.state[inputKey],
+                words = this.state[wordsKey];
 
             switch (e.which) {
                 case 38: // up key
                     e.preventDefault();
-                    if (this.state[tabKey]) {
-                        if (this.state[tabKey].index > 0) {
-                            --this.state[tabKey].index;
+                    if (words) {
+                        if (words.index > 0) {
+                            --words.index;
                             this.setState(this.state);
                         }
                     }
@@ -71,9 +99,9 @@ define(function (require, exports, module) {
 
                 case 40: // down key
                     e.preventDefault();
-                    if (this.state[tabKey]) {
-                        if (this.state[tabKey].index < this.state[tabKey].list.length - 1) {
-                            ++this.state[tabKey].index;
+                    if (words) {
+                        if (words.index < words.list.length - 1) {
+                            ++words.index;
                             this.setState(this.state);
                         }
                     }
@@ -81,63 +109,94 @@ define(function (require, exports, module) {
 
                 case 37: // left key
                 case 39: // right key
-                    // e.preventDefault();
-                    delete this.state[tabKey];
-                    this.setState(this.state);
-                    break;
-
-                case 9: // tab key
-                    e.preventDefault();
-                    this.state[tabKey] = {
-                        word: this.getCurrentWord(e),
-                        index: 0,
-                        list: [ ]
-                    };
+                    delete this.state[wordsKey];
                     this.setState(this.state);
                     break;
 
                 case 13: // enter key
-                    MessageActions.reply(this.props.server, this.props.channel, this.state[inputKey]);
-                    this.state[inputKey] = '';
-                    this.setState(this.state);
+                    if (words) {
+                        this.state[inputKey] = this.replaceCurrentWord(words.list[words.index]);
+                        delete this.state[wordsKey];
+                        this.setState(this.state);
+                    } else {
+                        MessageActions.reply(this.props.server, this.props.channel, input);
+                        this.state[inputKey] = '';
+                        this.setState(this.state);
+                    }
                     break;
             }
         },
 
         onKeyUp: function (e) {
-            var tabKey = this.getKey('tab');
-            if (this.state[tabKey]) {
-                this.state[tabKey].word = this.getCurrentWord(e);
-                this.state[tabKey].list = this.getTabList(this.state[tabKey].word);
+            var wordsKey = this.wordsKey(),
+                words = this.state[wordsKey],
+                word = this.currentWord();
+
+            if (words) {
+                if (word) {
+                    words.word = word.substr(1);
+                    words.list = this.wordList(words.word);
+
+                } else {
+                    delete this.state[wordsKey];
+
+                }
+                this.setState(this.state); 
+
+            } else if (word && e.which === 50) {
+                if (word[0] === '@') {
+                    word = word.substr(1);
+                    this.state[wordsKey] = {
+                        word: word,
+                        index: 0,
+                        list: this.wordList(word)
+                    };
+                } else {
+                    delete this.state[wordsKey];
+
+                }
                 this.setState(this.state); 
             }
         },
 
-        render: function () {
-            var tabKey = this.getKey('tab'),
-                tabUI = '';
+        onComplete: function (value, e) {
+            var inputKey = this.inputKey(),
+                wordsKey = this.wordsKey(),
+                input = this.state[inputKey],
+                words = this.state[wordsKey];
 
-            if (this.state[tabKey] &&
-                this.state[tabKey].list &&
-                this.state[tabKey].list.length) {
-                tabUI = <ul className="collection">
-                    {this.state[tabKey].list.map((user, index) => {
-                        var style = (index === this.state[tabKey].index) ?
-                            'collection-item active' : 'collection-item';
-                        return <li key={user} className={style}>{user}</li>;
+            e.preventDefault();
+            if (words) {
+                this.state[inputKey] = this.replaceCurrentWord(value);
+                delete this.state[wordsKey];
+                this.setState(this.state);
+            }
+        },
+
+        render: function () {
+            var wordsKey = this.wordsKey(),
+                words = this.state[wordsKey],
+                wordsUI = '';
+
+            if (words &&
+                words.list &&
+                words.list.length) {
+                wordsUI = <ul className="complete-list">
+                    {words.list.map((user, index) => {
+                        var style = (index === words.index) ?
+                            'active' : 'collection-item';
+                        return <li key={user} className={style}>
+                            <a href="#!" onClick={this.onComplete.bind(this, user)}>{user}</a></li>;
                     })}
                 </ul>;
             }
 
-            console.log(this.state);
-
-            // ref={inputRef}
             return <div className="message-reply">
-                {tabUI}
+                {wordsUI}
                 <div className="input-field">
                     <input type="text"
                         ref="reply"
-                        value={this.getValue('input')}
+                        value={this.stateValue('input', '')}
                         onChange={this.onChange}
                         onKeyDown={this.onKeyDown}
                         onKeyUp={this.onKeyUp} />
